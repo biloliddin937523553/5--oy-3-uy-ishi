f7650ea5df374290fb025715b5923953da034972
biloliddin937523553 <biloliddinlatipov0@gmail.com>
2024-03-29 21:35:56 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let ism = ('ism')
+let yosh = ('yosh')
+let city = ('Shahar')
+
+
+
+let myobj = {
+    name : 'John',
+    age : 30 ,
+    city : 'New York'
+}
+for(let element in myobj){
+    console.log(myobj[element]);
+}
